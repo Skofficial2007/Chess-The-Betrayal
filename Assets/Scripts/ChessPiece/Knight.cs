28d0c59e056f1855eae33fcf715aa121d915f3fc
Siddharth <skofficial20072003@gmail.com>
2025-11-10 09:07:21 +0530

    Chess Pieces Setup and Dragging System

---
+++ Assets/Scripts/ChessPiece/Knight.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ChessTheMasterPiece.ChessPiece
+{
+    public class Knight : ChessPiece
+    {
+
+    }
+}
---
+++ Assets/Scripts/ChessPiece/Queen.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ChessTheMasterPiece.ChessPiece
+{
+    public class Queen : ChessPiece
+    {
+
+    }
+}
